a9ab7408f428c1cd13d9170a721c6bedb1236abb
Zach <zmeisne1@jh.edu>
2024-09-04 22:56:52 -0400

    removing typescript and react from cjs file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,21 @@
 module.exports = {
-    env: {
-      browser: true, // if you're writing code that runs in the browser
-      node: true,    // for Node.js environment
-      es2021: true,  // support for ECMAScript 2021
-    },
-    extends: [
-      'eslint:recommended', // Use recommended ESLint rules
-      'plugin:@typescript-eslint/recommended', // If using TypeScript
-      'plugin:react/recommended', // For React projects
-    ],
-    parser: '@typescript-eslint/parser', // Enable parsing for TypeScript files
-    parserOptions: {
-      ecmaVersion: 12, // ECMAScript version
-      sourceType: 'module', // Use ES6 module system
-      ecmaFeatures: {
-        jsx: true, // Enable JSX parsing
-      },
-    },
-    plugins: [
-      'react', // React plugin
-      '@typescript-eslint', // TypeScript plugin
-    ],
-    rules: {
-      // Example rules, customize these based on your project needs
-      'no-unused-vars': 'warn', // Warn about unused variables
-      'react/react-in-jsx-scope': 'off', // Disable this rule if using React 17+
-      '@typescript-eslint/no-explicit-any': 'off', // Allow 'any' type in TypeScript
-    },
-    settings: {
-      react: {
-        version: 'detect', // Automatically detect the React version
-      },
-    },
-  };
-  
+  env: {
+    browser: true, // Set to true if you're targeting browser environments
+    node: true,    // Set to true if you're targeting Node.js environments
+    es2021: true,  // Enable ECMAScript 2021 features
+  },
+  extends: [
+    'eslint:recommended', // Use the recommended ESLint rules
+  ],
+  parserOptions: {
+    ecmaVersion: 12, // Use ECMAScript 2021 syntax
+    sourceType: 'module', // Enable ES6 modules
+  },
+  rules: {
+    // Add or override rules here
+    'no-console': 'warn', // Warn on console.log statements
+    'no-unused-vars': 'warn', // Warn on unused variables
+    'semi': ['error', 'always'], // Enforce semicolons
+    'quotes': ['error', 'single'], // Enforce single quotes
+  },
+};
